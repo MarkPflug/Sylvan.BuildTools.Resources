f88c75dc76a740484f8e9eb0cf3df578fd4aa733
MarkPflug <MarkPflug@users.noreply.github.com>
2017-10-09 17:05:42 -0700

    Json resource generator baseline

---
+++ Elemental.JsonResource/JsonResourceGenerator.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Elemental.JsonResource
+{
+
+    public class JsonResourceGenerator : Task
+    {
+        [Required]
+        // The folder where we will write all of our generated code.
+        public String OutputPath { get; set; }
+
+        // All of the .resj files in our projct.
+        public ITaskItem[] InputFiles { get; set; }
+
+        // Will contain all of the generated coded we create
+        [Output]
+        public ITaskItem[] OutputCode { get; set; }
+
+        // Will contain all of the resources we generate.
+        [Output]
+        public ITaskItem[] OutputResources { get; set; }
+
+        // The method that is called to invoke our task.
+        public override bool Execute()
+        {
+            var outCodeItems = new List<TaskItem>();
+            var outResItems = new List<TaskItem>();
+
+            // loop over all the .resj files we were given
+            foreach (var iFile in InputFiles)
+            {
+                // load the Json from the file
+                var text = File.ReadAllText(iFile.ItemSpec);
+                var obj = JObject.Parse(text);
+
+                // prepare the generated files we are about to write.
+                var codeFile = Path.Combine(OutputPath, iFile.ItemSpec + ".g.cs");
+                var resFile = Path.Combine(OutputPath, iFile.ItemSpec + ".resources");
+
+                outCodeItems.Add(new TaskItem(codeFile));
+                outResItems.Add(new TaskItem(resFile));
+
+                Directory.CreateDirectory(Path.GetDirectoryName(codeFile));
+                string resName = Path.GetFileName(iFile.ItemSpec);
+                string className = Path.GetFileNameWithoutExtension(iFile.ItemSpec);
+
+                // write the generated C# code and resources file.
+                using (var w = new StreamWriter(codeFile))
+                using (var rw = new System.Resources.ResourceWriter(resFile))
+                {
+                    // very simplistic resource accessor class mostly duplicated from resx output.
+                    w.WriteLine("public static partial class " + className + " { ");
+                    w.WriteLine("static global::System.Resources.ResourceManager rm;");
+                    w.WriteLine("static global::System.Globalization.CultureInfo resourceCulture;");
+                    w.WriteLine("static global::System.Resources.ResourceManager ResourceManager {");
+                    w.WriteLine("get {");
+                    w.WriteLine("if(object.ReferenceEquals(rm, null)) {");
+                    w.WriteLine("rm = new global::System.Resources.ResourceManager(\"" + resName + "\", typeof(" + className + ").Assembly);");
+                    w.WriteLine("}");
+                    w.WriteLine("return rm;");
+                    w.WriteLine("}");
+                    w.WriteLine("}");
+                    w.WriteLine("static global::System.Globalization.CultureInfo Culture {");
+                    w.WriteLine("get {");
+                    w.WriteLine("return resourceCulture;");
+                    w.WriteLine("}");
+                    w.WriteLine("set {");
+                    w.WriteLine("resourceCulture = value;");
+                    w.WriteLine("}");
+                    w.WriteLine("}");
+
+                    // loop over all the strings in our resj file.
+                    foreach (var kvp in (JObject)obj["Strings"])
+                    {
+                        var key = kvp.Key;
+                        var value = (JValue)kvp.Value;
+
+                        // write our string to the resources binary
+                        rw.AddResource(key, (string)value.Value);
+
+                        // generate a C# property to access the string by name.
+                        w.WriteLine("public static string " + key + " {");
+                        w.WriteLine("get {");
+                        w.WriteLine("return ResourceManager.GetString(\"" + key + "\", resourceCulture);");
+                        w.WriteLine("}");
+                        w.WriteLine("}");
+                    }
+
+                    w.WriteLine("}");
+                }
+            }
+            // put the artifacts we created in the output properties.
+            OutputCode = outCodeItems.ToArray();
+            OutputResources = outResItems.ToArray();
+            return true;
+        }
+    }
+}
